b47d916919a1a8c25ce3ea44cae91ac82cdbd0b7
LuJun <jun.lu@hitisoft.com>
2019-01-04 16:04:25 +0800

    dsada

--- cache/sw-demo-cache.js
+++ cache/sw-demo-cache.js
@@ -1,4 +1,4 @@
-var VERSION = 'v10';
+var VERSION = 'v11';
 
 // 缓存
 self.addEventListener('install', function(event) {
@@ -36,45 +36,46 @@ self.addEventListener('activate', function(event) {
 });
 
 // 捕获请求并返回缓存数据
-// self.addEventListener('fetch', function(event) {
-//   console.log(44444)
-//   event.respondWith(caches.match(event.request).catch(function() {
-//     return fetch(event.request);
-//   }).then(function(response) {
-//     caches.open(VERSION).then(function(cache) {
-//       cache.put(event.request, response);
-//     });
-//     return response.clone();
-//   }).catch(function() {
-//     return caches.match('./static/mm1.jpg');
-//   }));
-// });
-self.addEventListener('fetch', function(e) {
-  console.log('Fetch event ' + VERSION + ' :', e.request.url);
-  e.respondWith( // 该策略先从网络中获取资源，如果获取失败则再从缓存中读取资源
-    fetch(e.request.url)
-    .then(function (httpRes) {
+self.addEventListener('fetch', function(event) {
+  console.log('fetch')
+  console.log(VERSION)
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(response) {
+    caches.open(VERSION).then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function() {
+    return caches.match('./static/mm1.jpg');
+  }));
+});
+// self.addEventListener('fetch', function(e) {
+//   console.log('Fetch event ' + VERSION + ' :', e.request.url);
+//   e.respondWith( // 该策略先从网络中获取资源，如果获取失败则再从缓存中读取资源
+//     fetch(e.request.url)
+//     .then(function (httpRes) {
 
-      // 请求失败了，直接返回失败的结果
-      if (!httpRes || httpRes.status !== 200) {
-          // return httpRes;
-          return caches.match(e.request)
-      }
+//       // 请求失败了，直接返回失败的结果
+//       if (!httpRes || httpRes.status !== 200) {
+//           // return httpRes;
+//           return caches.match(e.request)
+//       }
 
-      // 请求成功的话，将请求缓存起来。
-      var responseClone = httpRes.clone();
-      caches.open(VERSION).then(function (cache) {
-          return cache.delete(e.request)
-          .then(function() {
-              cache.put(e.request, responseClone);
-          });
-      });
+//       // 请求成功的话，将请求缓存起来。
+//       var responseClone = httpRes.clone();
+//       caches.open(VERSION).then(function (cache) {
+//           return cache.delete(e.request)
+//           .then(function() {
+//               cache.put(e.request, responseClone);
+//           });
+//       });
 
-      return httpRes;
-    })
-    .catch(function(err) { // 无网络情况下从缓存中读取
-      console.error(err);
-      return caches.match(e.request);
-    })
-  )
-})
+//       return httpRes;
+//     })
+//     .catch(function(err) { // 无网络情况下从缓存中读取
+//       console.error(err);
+//       return caches.match(e.request);
+//     })
+//   )
+// })
